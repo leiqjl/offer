714c36dcf3a7849e62a44035b5bc741e4fd07e90
leiqjl <leiqjl@gmail.com>
2017-10-29 15:09:22 +0800

    工具类

---
+++ src/com/code/util/ListNode.java
@@ -0,0 +1,14 @@
+package com.code.util;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this(val,null);
+    }
+    public ListNode(int val,ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
