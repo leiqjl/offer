a6c4895cfcc8d43b68ff1aa4e9be87546a979c14
leiqjl <leiqjl@gmail.com>
2017-11-05 20:49:35 +0800

    链表节点类

---
+++ src/com/code/ListNode.java
@@ -0,0 +1,14 @@
+package com.code;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this(val,null);
+    }
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
