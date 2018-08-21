28fc9aa810503b2736f123428c0bae0f3f8e5d59
leiqjl <leiqjl@gmail.com>
2018-08-21 22:10:51 +0800

    合并两个排序的链表

--- src/com/code/Problem17.java
+++ src/com/code/Problem17.java
@@ -3,7 +3,7 @@ package com.code;
 /**
  * 输入两个递增排序的链表，合并这两个链表并使新链表中的结点仍然是按照递增排序的。
  */
-public class Problem16 {
+public class Problem17 {
 
     public ListNode merge(ListNode head1, ListNode head2) {
         if (head1 == null) {
